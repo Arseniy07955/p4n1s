74dfc7b63bd70e0ec46bc64e141c1cf951cb6119
Arseniy07955 <arseeenalpro8899@gmail.com>
2025-03-20 16:58:07 +0300

    new installer

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
-#! /bin/bash
+#!/bin/bash
+
+if [ $EUID -ne 0 ]; then
+    echo "Please run this script as root or with sudo."
+    exit 1
+fi
+
 g++ main.cpp -o penis
 cp penis /bin/
 rm penis
+echo "I guess its installed now! Run penis to use it :)"
